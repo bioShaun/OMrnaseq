53223d5839c0277ffb3bdccefc187a522a630237
BioShuan <ricekent@163.com>
2017-07-11 18:30:18 +0800

    rm merge conflit

--- rnaseq/utils/util_functions.py
+++ rnaseq/utils/util_functions.py
@@ -4,31 +4,6 @@ import os
 from . import config
 
 
-<<<<<<< HEAD
-class prepare_dir(luigi.Task):
-
-    proj_dir = luigi.Parameter()
-    _module = None
-
-    def run(self):
-        with self.output().open('w') as prepare_dir_log:
-            for each_module in config.module_dir[self._module]:
-                each_module_dir = os.path.join(
-                    self.proj_dir, config.module_dir[self._module][each_module])
-                config.module_dir[self._module][each_module] = each_module_dir
-                try:
-                    os.makedirs(each_module_dir)
-                except OSError:
-                    prepare_dir_log.write(
-                        '{each_module_dir} has been built before.'.format(**locals()))
-
-    def output(self):
-        return luigi.LocalTarget('{t.proj_dir}/{log_dir}/prepare_dir.log'.format(
-            t=self, log_dir=config.module_dir[self._module]['logs']))
-
-
-=======
->>>>>>> 4e136c66d0d8b2f7604b5a1b2d3b063655e6f6a7
 def rsync_pattern_to_file(from_dir, pattern_list):
     pattern_path_list = [
         '{0}/{1}'.format(from_dir, each_pattern) for each_pattern in pattern_list]
